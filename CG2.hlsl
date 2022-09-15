02b8c22bacca81f270d65078db0d6918099c6249
Rafaela <rafalasantosdarosa@yahoo.com>
2022-09-15 11:41:41 -0300

    Criação dos métodos vertex e fragment

--- CG2.hlsl
+++ CG2.hlsl
@@ -2,9 +2,14 @@ sampler s0 : register(s0);
 
 float4 main(float2 tex : TEXCOORD0) : COLOR{
     float4 rgb = tex2D(s0, tex);
-    float4 res = float4((rgb[0] + rgb[1] + rgb[2])/3,
-                         (rgb[0] + rgb[1] + rgb[2])/3, 
-                         (rgb[0] + rgb[1] + rgb[2])/3, 1); // realiza a conversão para tons de cinza
 
-    return res;
+    // float4 res = float4((rgb[0] + rgb[1] + rgb[2])/3,
+    //                      (rgb[0] + rgb[1] + rgb[2])/3, 
+    //                      (rgb[0] + rgb[1] + rgb[2])/3, 1); // realiza a conversão para tons de cinza
+
+    if(1.1*(rgb[0] + rgb[2]) - rgb[1] < 0){
+            rgb = float4(tex[0], 0, tex[1], 1); // 0 = x | y = 1
+    }
+
+    return rgb;
 } 
--- Render.js
+++ Render.js
@@ -16,10 +16,49 @@ class Render{
             console.error("Erro ao iniciar o WebGL");
             return;
         }
+
+        var vertexShaderSource = document.getElementById("meu-vertex-shader").textContent;
+        var fragmentShaderSource = document.getElementById("meu-fragment-shader").textContent;
+        var vertexShader = Render.createShader(this.gl, this.gl.VERTEX_SHADER, vertexShaderSource);
+        var fragmentShader = Render.createShader(this.gl, this.gl.FRAGMENT_SHADER, vertexShaderSource);
+
+        this.program = Render.createProgram(this.gl, vertexShader, fragmentShader);
     }
 
     draw(){
         this.gl.clearColor(0, 0, 0, 1);
         this.gl.clear(gl.COLOR_BUFFER_BIT);
     }
+
+    static createShader(gl, type, source){
+        var shader = gl.creteShader(type);
+        gl.shaderSource(shader, source);
+        g1.compilerShader(shader);
+
+        var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+        if(success){
+            return shader;
+        }
+
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    }
+
+    static createProgram(gl, vertexShader, fragmentShader){
+        var program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        var success = gl.getProgramParamenter(program, gl.LINK_STATUS);
+
+        if(success){
+            return program;
+        }
+        
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+
 }
