a9cf9a64bf47f6ee8074b7b0b6119b24ca888a46
Rafaela <rafalasantosdarosa@yahoo.com>
2022-09-22 11:02:07 -0300

    Criado matrizes pra definir transformações dos buffers de vértices para as coordenadas de tela - aula 22/09

--- Render.js
+++ Render.js
@@ -17,23 +17,54 @@ class Render{
             return;
         }
 
-        var vertexShaderSource = document.getElementById("meu-vertex-shader").textContent;
-        var fragmentShaderSource = document.getElementById("meu-fragment-shader").textContent;
+        var vertexShaderSource = document.getElementById("meu-vertex-shader").text;
+        var fragmentShaderSource = document.getElementById("meu-fragment-shader").text;
         var vertexShader = Render.createShader(this.gl, this.gl.VERTEX_SHADER, vertexShaderSource);
-        var fragmentShader = Render.createShader(this.gl, this.gl.FRAGMENT_SHADER, vertexShaderSource);
+        var fragmentShader = Render.createShader(this.gl, this.gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-        this.program = Render.createProgram(this.gl, vertexShader, fragmentShader);
+        this.programa = Render.createProgram(this.gl, vertexShader, fragmentShader);
+        this.positionAttributeLocation = this.gl.getAttribLocation(this.programa, "posicao");
+        this.positionBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+
+        this.gl.useProgram(this.programa);
+        this.matrixLocation = this.gl.getUniformLocation(this.programa, "u_escala");
+        this.matriz = [this.canvas.height / this.canvas.width, 0, 0, 0,
+                                                            0, 1, 0, 0,
+                                                            0, 0, 1, 0,
+                                                            0, 0, 0, 1];
     }
 
     draw(){
         this.gl.clearColor(0, 0, 0, 1);
-        this.gl.clear(gl.COLOR_BUFFER_BIT);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        this.gl.useProgram(this.programa);
+
+        this.gl.uniformMatrix4fv(this.matrixLocation, false, this.matriz);
+
+        var positions = [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0];
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
+        this.gl.enableVertexAttribArray(this.positionAttributeLocation);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+
+        var size = 3;
+        var type = this.gl.FLOAT;
+        var normalize = false;
+        var stride = 0;
+        var offset = 0;
+        this.gl.vertexAttribPointer(this.positionAttributeLocation, size, type, normalize, stride, offset);
+
+        var primitiveType = this.gl.TRIANGLES;
+        var offset = 0;
+        var count = 3;
+        this.gl.drawArrays(primitiveType, offset, count);
+       
     }
 
     static createShader(gl, type, source){
-        var shader = gl.creteShader(type);
+        var shader = gl.createShader(type);
         gl.shaderSource(shader, source);
-        g1.compilerShader(shader);
+        gl.compileShader(shader);
 
         var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
 
@@ -51,7 +82,7 @@ class Render{
         gl.attachShader(program, fragmentShader);
         gl.linkProgram(program);
 
-        var success = gl.getProgramParamenter(program, gl.LINK_STATUS);
+        var success = gl.getProgramParameter(program, gl.LINK_STATUS);
 
         if(success){
             return program;
--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 "use strict";
-var canvas = document.getElementById("tela");
-var gl = canvas.getContext("webgl");
-function main(){
-    if(!gl){
-        console.error("Navegador não suporta WebGL");
-        return;
-    }
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+var render = new Render("tela");
 
-
+function desenhar(){
+    render.draw();
+    requestAnimationFrame(desenhar);
 }
 
-main();
+requestAnimationFrame(desenhar);
